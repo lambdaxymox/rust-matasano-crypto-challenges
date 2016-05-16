4ee6f685bd44cb188981f9e6271b548ff67e9b2f
LambdaXymox <lambda.xymox@gmail.com>
2016-05-16 18:45:14 -0500

    Code refactoring.

--- src/set1/challenge2.rs
+++ src/set1/challenge2.rs
@@ -1,11 +1,28 @@
 use bitwise::bitwiseops;
 use bitwise::hex_rep::ToHexRep;
+use challengeinfo::challenge::{Challenge, ChallengeInfo};
 
-fn challenge2() {
+
+pub const INFO2: ChallengeInfo<'static> = ChallengeInfo {
+    set_number: 2,
+    challenge_number: 2,
+    title: "Fixed XOR",
+    description: "",
+    url: "http://cryptopals.com/sets/1/challenges/2",
+};
+
+pub const CHALLENGE2: Challenge<'static> = Challenge {
+    info: INFO2,
+    func: execute,
+};
+
+fn execute() -> String {
     let hex1  = String::from("1c0111001f010100061a024b53535009181c").to_hex().unwrap();
     let hex2  = String::from("686974207468652062756c6c277320657965").to_hex().unwrap();
     let result   = bitwiseops::exact_block_xor(hex1.as_slice(), hex2.as_slice()).unwrap();
     let expected = String::from("746865206b696420646f6e277420706c6179").to_hex().unwrap();
 
     assert_eq!(expected, result);
+
+    String::from_utf8(result).unwrap()
 }
--- src/set1/challenge3.rs
+++ src/set1/challenge3.rs
@@ -1,16 +1,31 @@
 use cryptanalysis::english;
 use bitwise::bitwiseops;
 use bitwise::hex_rep::ToHexRep;
+use challengeinfo::challenge::{Challenge, ChallengeInfo};
 
 
-fn challenge3() {
-    let cipher_text = String::from_utf8(String::from("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
-                             .to_hex().unwrap()).unwrap();
-    let guessed_char = english::most_likely_char(&cipher_text);
-    let plain_text = String::from_utf8(bitwiseops::xor_with_char(guessed_char, cipher_text.as_ref())).unwrap();
+pub const INFO3: ChallengeInfo<'static> = ChallengeInfo {
+    set_number: 3,
+    challenge_number: 3,
+    title: "Single-byte XOR cipher",
+    description: "",
+    url: "http://cryptopals.com/sets/1/challenges/3",
+};
 
-    let expected = String::from("Cooking MC's like a pound of bacon");
+pub const CHALLENGE3: Challenge<'static> = Challenge {
+    info: INFO3,
+    func: execute,
+};
 
-    assert_eq!(expected.len(), plain_text.len());
-    assert_eq!(expected, plain_text);
+fn execute() -> String {
+    let ciphertext   = String::from("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+                              .to_hex().unwrap();
+    let guessed_char = english::most_likely_byte(&ciphertext);
+    let plaintext    = bitwiseops::xor_with_char(guessed_char, ciphertext.as_ref());
+    let expected_plaintext = Vec::from("Cooking MC's like a pound of bacon");
+
+    assert_eq!(expected_plaintext.len(), plaintext.len());
+    assert_eq!(expected_plaintext, plaintext);
+
+    String::from_utf8(expected_plaintext).unwrap()
 }
