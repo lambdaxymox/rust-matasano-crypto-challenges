f040c3195f6433be9e2fdaa9264044fdee035d8e
LambdaXymox <lambda.xymox@gmail.com>
2016-05-16 17:23:03 -0500

    Add unit test.

--- src/cryptanalysis/tests.rs
+++ src/cryptanalysis/tests.rs
@@ -18,3 +18,39 @@ fn test_most_likely_byte() {
     assert_eq!(key, guessed_key);
     assert_eq!(plaintext.as_slice(), guessed_plaintext.as_slice());
 }
+
+#[test]
+fn test_score_with() {
+    use num_rational::Ratio;
+    use std::collections::HashMap;
+    use super::frequency_analysis::Histogram;
+    use std::convert::From;
+
+    fn make_table(array: &[(u8, usize)]) -> (usize, HashMap<u8,usize>) {
+        let mut table = HashMap::new();
+        let mut total = 0;
+
+        for ref kv in array {
+            table.insert(kv.0, kv.1);
+            total += kv.1;
+        }
+
+        (total, table)
+    }
+
+    let plaintext = Vec::from("Cooking MC\'s like a pound of bacon");
+    let (total, table): (usize, HashMap<u8,usize>)
+         = make_table(english::FREQUENCY_LIST.as_ref());
+
+    let model     = Histogram::from(english::FREQUENCY_LIST.as_ref());
+    let score     = break_xor_cipher::score_with(&model, plaintext.as_ref());
+
+    let mut expected_score = Ratio::from_integer(0);
+    for ch in plaintext.into_iter() {
+        if table.contains_key(&ch) {
+            expected_score = expected_score + Ratio::new_raw(*table.get(&ch).unwrap(), total);
+        }
+    }
+
+    assert_eq!(expected_score, score);
+}
